222423d3c38eaa225bf6ce2df364507e51e30686
phuong0 <44624226+phuong0@users.noreply.github.com>
2019-01-31 16:46:05 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#b1").click(function(){
+    $("#banhcuon").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#b2").click(function(){
+    $("#bunbo").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#b3").click(function(){
+    $("#hutieu").toggle();
+});
+
+
+
